93c8ad47680288485b619483438f4004f588fe10
João Pedro Alcalde <alcaldeguima@gmail.com>
2021-11-09 02:23:29 -0300

    pegando o state do form WIP PopUp

--- src/Components/main/form/form.jsx
+++ src/Components/main/form/form.jsx
@@ -1,28 +1,53 @@
-import { Component } from "react";
+import React, { useState } from "react";
 import "../../header/header.css";
 
-class Form extends Component {
-  render() {
-    return (
-      <>
-        <div className="contato"></div>
-        <div className="contact">
-          <div className="center">
-            <form>
-              <h2>Contato</h2>
-              <input
-                type="text"
-                name="name"
-                id="name"
-                placeholder="Nome Completo"
-              />
-              <input type="email" name="email" id="email" placeholder="Email" />
-              <input type="submit" value="Enviar" />
-            </form>
-          </div>
+function Form() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [text, setText] = useState("");
+
+  return (
+    <>
+      <div className="contato"></div>
+      <div className="contact">
+        <div className="center">
+          <form
+            onSubmit={(event) => {
+              event.preventDefault();
+              console.log(name, email, text);
+            }}
+          >
+            <h2>Contato</h2>
+            <input
+              value={name}
+              onChange={(event) => setName(event.target.value)}
+              type="text"
+              name="name"
+              id="name"
+              placeholder="Nome Completo"
+            />
+            <input
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Email"
+            />
+
+            <input
+              value={text}
+              onChange={(event) => setText(event.target.value)}
+              type="text"
+              name="text"
+              id="text"
+              placeholder="Digite sua mensagem"
+            />
+            <input type="submit" value="Enviar" />
+          </form>
         </div>
-      </>
-    );
-  }
+      </div>
+    </>
+  );
 }
 export default Form;
