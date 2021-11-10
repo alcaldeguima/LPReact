11689bf22062e4a2bdd1c2ee3f88c5bb9168032b
Lucas <lucas-sdoliveira12@educar.rs.gov.br>
2021-11-10 17:35:45 -0300

    Erros resolvidos na iteração feita nos membros

--- src/Components/MembersPersonal/MembersPersonal.jsx
+++ src/Components/MembersPersonal/MembersPersonal.jsx
@@ -12,42 +12,48 @@ class MembersPersonal extends Component {
     render() { 
         return (
             <>
-                <div id={this.integrantes.scroll} className="scroll-to-member"></div>
-                
-                <div className={this.integrantes.background}>
-                    <div className="center">
-                        <div className={this.integrantes.colunas}>
-                            <div className="member-image">
-                                <div className="circle">
-                                    <img src={this.integrantes.src}/>
-                                </div>
-                            </div>
-                            <div className="member-information">
-                                <h2>{this.integrantes.nome}</h2>
-                                <div className="line-heading" id={this.integrantes.id}>
-                                </div>
-                                <p>{this.integrantes.text}</p>
-                                <div className="social-media">
-                                    <a href="https://www.linkedin.com/in/jo%C3%A3o-pedro-alcalde-guimar%C3%A3es-617523208/" target="_blank">
-                                        <div className="social-media-icon">
-                                            <img src={linkedin} alt="LinkedIn Logo"/>
+            {this.integrantes.map (integrante => {
+                return(
+                    <section>
+                        <div id={integrante.scroll} className="scroll-to-member"></div>
+                    
+                        <div className={integrante.background}>
+                            <div className="center">
+                                <div className={integrante.colunas}>
+                                    <div className="member-image">
+                                        <div className="circle">
+                                            <img src={integrante.src}/>
                                         </div>
-                                    </a>
-                                    <a href="https://github.com/alcaldeguima" target="_blank">
-                                        <div className="social-media-icon">
-                                            <img src={github} alt="GitHub Logo"/>
+                                    </div>
+                                    <div className="member-information">
+                                        <h2>{integrante.nome}</h2>
+                                        <div className="line-heading" id={integrante.id}>
                                         </div>
-                                    </a>
-                                    <a href="#" target="_blank">
-                                        <div className="social-media-icon">
-                                            <img src={instagram} alt="Instagram Logo"/>
+                                        <p>{integrante.text}</p>
+                                        <div className="social-media">
+                                            <a href="https://www.linkedin.com/in/jo%C3%A3o-pedro-alcalde-guimar%C3%A3es-617523208/" target="_blank">
+                                                <div className="social-media-icon">
+                                                    <img src={linkedin} alt="LinkedIn Logo"/>
+                                                </div>
+                                            </a>
+                                            <a href="https://github.com/alcaldeguima" target="_blank">
+                                                <div className="social-media-icon">
+                                                    <img src={github} alt="GitHub Logo"/>
+                                                </div>
+                                            </a>
+                                            <a href="#" target="_blank">
+                                                <div className="social-media-icon">
+                                                    <img src={instagram} alt="Instagram Logo"/>
+                                                </div>
+                                            </a>
                                         </div>
-                                    </a>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                </div>
+                    </section>
+                );
+            })}
         </>
         );
     }
