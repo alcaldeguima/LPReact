366596b05893b7ab696e534a0fd32799908c7122
Lucas <lucas-sdoliveira12@educar.rs.gov.br>
2021-11-09 21:09:20 -0300

    propriedade class, alterada para className

--- src/Components/main/intro/intro.jsx
+++ src/Components/main/intro/intro.jsx
@@ -5,11 +5,11 @@ class Intro extends Component {
   render() {
     return (
       <div className="introduction">
-        <div class="center">
+        <div className="center">
           <h1>
             Conheça a <strong>Dev Stuy</strong>
           </h1>
-          <div class="line-heading"></div>
+          <div className="line-heading"></div>
 
           <p>
             Somos um coletivo formado por alunos bolsistas do projeto
