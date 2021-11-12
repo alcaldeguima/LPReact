ec2e102b31b86f08c66cb8f7b2150936ef77a2ac
Pedro Zardin <pezazagui@gmail.com>
2021-11-12 05:14:21 -0300

    Teste para arrumar bug das pastas footer e header

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import "./header.css";
+
+class Header extends Component {
+  render() {
+    return (
+      <header>
+        <nav className="center two-columns" id="#">
+          <h1>Dev Stuy</h1>
+          <ul>
+            <li>
+              <a href="#">
+                Sobre
+                <div className="line-heading"></div>
+              </a>
+            </li>
+            <li>
+              <a href="#membros">
+                Membros
+                <div className="line-heading"></div>
+              </a>
+            </li>
+            <li>
+              <a href="#contato">
+                Comentários
+                <div className="line-heading"></div>
+              </a>
+            </li>
+          </ul>
+        </nav>
+        <div className="line-header"></div>
+      </header>
+    );
+  }
+}
+export default Header;
